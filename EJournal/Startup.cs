908e78f1b8500aaad3ce9469865b5791b12b061e
Nikita Nikitich <danik22122005@gmail.com>
2020-03-11 13:52:06 +0200

    added branch

--- EJournal/Startup.cs
+++ EJournal/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace EJournal
-{
+{ 
     public class Startup
     {
         public Startup(IConfiguration configuration)
