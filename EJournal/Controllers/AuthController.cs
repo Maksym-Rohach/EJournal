68ba7bb96e568020c14fbddb412ed2507308e49d
Nikita Nikitich <danik22122005@gmail.com>
2020-03-16 12:22:15 +0200

    added login back

---
+++ EJournal/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using EJournal.Data.EfContext;
+using EJournal.Data.Entities.AppUeser;
+using EJournal.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EJournal.Controllers
+{
+    [Route("api/[controller]")]
+    //[ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<DbUser> _userManager;
+        private readonly SignInManager<DbUser> _signInManager;
+        private readonly EfDbContext _context;
+        public AuthController(EfDbContext context, UserManager<DbUser> userManager, SignInManager<DbUser> sigInManager)
+        {
+            _userManager = userManager;
+            _signInManager = sigInManager;
+            _context = context;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login([FromBody] LoginModel model)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == model.Email);
+            if (user == null)
+            {
+                return "Не правильна електронна пошта!";
+            }
+            var res = _signInManager
+                .PasswordSignInAsync(user, model.Password, false, false).Result;
+            if (!res.Succeeded)
+            {
+                return "Не правильний пароль!";
+            }
+
+            await _signInManager.SignInAsync(user, isPersistent: false);
+            return await CreateTokenAsync(user);
+        }
+
+        private async Task<string> CreateTokenAsync(DbUser user)
+        {
+            List<Claim> claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+            };
+
+            var now = DateTime.UtcNow;
+            var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("secret-key-example"));
+            var signinCredentials = new SigningCredentials(signinKey, SecurityAlgorithms.HmacSha256);
+
+            var jwt = new JwtSecurityToken(
+                signingCredentials: signinCredentials,
+                expires: now.Add(TimeSpan.FromDays(1)),
+                claims: claims
+                );
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+    }
+}
