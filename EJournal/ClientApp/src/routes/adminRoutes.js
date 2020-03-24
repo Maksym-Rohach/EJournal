ca1adb8ef6afce1af0d626c133a499c9b76c6291
Maks <m.rogach777@gmail.com>
2020-03-24 17:19:01 +0200

    add core UI

--- EJournal/ClientApp/src/routes/adminRoutes.js
+++ EJournal/ClientApp/src/routes/adminRoutes.js
@@ -1,24 +1,15 @@
 import React from 'react';
 
-const PersonsChart = React.lazy(() => import("../views/adminViews/PersonsChart"));
-const CommentsChart = React.lazy(() => import("../views/adminViews/CommentsChart"));
-//import CommentsChart from "../views/CommentsChart";
+const Login = React.lazy(() => import('../views/defaultViews/LoginPage'));
+const CommentsChart = React.lazy(() => import('../views/adminViews/CommentsChart'));
+const PersonsChart = React.lazy(() => import('../views/adminViews/PersonsChart'));
 
-var routes = [
+// https://github.com/ReactTraining/react-router/tree/master/packages/react-router-config
+const routes = [
+  { path: '/admin', exact: true, name: 'Login', component: Login },
+  { path: '/admin/clients', exact: true, name: 'Login', component: PersonsChart },
+  { path: '/admin/comments', exact: true, name: 'Login', component: CommentsChart },
   
-    {
-      path: "/persons",
-      name: "Persons",
-      icon: "tim-icons icon-single-02",
-      component: PersonsChart,
-      layout: "/admin"
-    },
-    {
-      path: "/comments",
-      name: "Comments",
-      icon: "tim-icons icon-pencil",
-      component: CommentsChart,
-      layout: "/admin"
-    }
 ];
+
 export default routes;
