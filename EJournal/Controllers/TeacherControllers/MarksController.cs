e616c2a709e21b7bef7b6294dcc11890ac6964bd
angelina <angelyusha.u@gmail.com>
2020-03-31 13:09:48 +0300

    httpget; httppost

--- EJournal/Controllers/TeacherControllers/MarksController.cs
+++ EJournal/Controllers/TeacherControllers/MarksController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using EJournal.Data.EfContext;
 using EJournal.Data.Entities.AppUeser;
+using EJournal.Services;
 using EJournal.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -19,18 +21,31 @@ namespace EJournal.Controllers.TeacherControllers
     {
         private readonly EfDbContext _context;
         private readonly UserManager<DbUser> _userManager;
+        private readonly IJwtTokenService _jwtTokenService;
 
-        public MarksController(EfDbContext context, UserManager<DbUser> userManager)
+        public MarksController(EfDbContext context, UserManager<DbUser> userManager, IJwtTokenService jwtTokenService)
         {
             _context = context;
             _userManager = userManager;
+            _jwtTokenService = jwtTokenService;
         }
 
+        //[HttpGet("teacher/getmarks")]
+        //public ContentResult GetMarksCurator()
+        //{
+
+        //}
+
         [HttpPost("teacher/getmarks")]
-        public IActionResult GetMarksCurator(GetMarksViewModel model)
+        public async Task<ActionResult<string>> GetMarksCurator([FromBody] GetMarksViewModel model)
         {
             try
             {
+                if (!ModelState.IsValid)
+                {
+                    return "Виберіть предмет";
+                }
+                var userId = User.FindFirstValue("id");
                 return Ok();
             }
             catch(Exception ex)
