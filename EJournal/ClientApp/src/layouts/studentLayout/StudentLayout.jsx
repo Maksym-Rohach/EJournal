e44a24fc2fd743543cc0151b9129f6648e171550
Maks <m.rogach777@gmail.com>
2020-04-02 16:20:59 +0300

    add photo in token

--- EJournal/ClientApp/src/layouts/studentLayout/StudentLayout.jsx
+++ EJournal/ClientApp/src/layouts/studentLayout/StudentLayout.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import SideBar from "./StudentSideBar";
 import {Redirect, Route, Switch } from "react-router-dom";
 import routes from "../../routes/adminRoutes";
-
-
+import { connect } from "react-redux";
+import get from "lodash.get";
+import { logout } from '../../views/defaultViews/LoginPage/reducer';
 
 class StudentLayout extends React.Component {
     // constructor(props) {
