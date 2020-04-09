b18e32149a3af8ced83d66ba84e008e612e208b1
angelina <angelyusha.u@gmail.com>
2020-04-09 10:58:09 +0300

    marksmodels and markscontroller

--- EJournal/Controllers/TeacherControllers/MarksController.cs
+++ EJournal/Controllers/TeacherControllers/MarksController.cs
@@ -16,7 +16,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EJournal.Controllers.TeacherControllers
 {
-    //[Authorize(Roles = "Teacher")]
+    [Authorize(Roles = "Teacher")]
     [Produces("application/json")]
     [Route("api/[controller]")]
     public class MarksController : ControllerBase
@@ -60,30 +60,81 @@ namespace EJournal.Controllers.TeacherControllers
         }
 
         [HttpPost("teacher/getmarks")]
-        public async Task<ActionResult<string>> GetMarksCurator([FromBody] GetMarksViewModel model)
+        public IActionResult GetMarksCurator([FromBody] MarksFilterModel model)
         {
-            try
+            //try
+            //{
+            //    if (!ModelState.IsValid)
+            //    {
+            //        return "Виберіть предмет або місяць";
+            //    }
+            //    var claims = User.Claims;
+            //    var userId = claims.FirstOrDefault().Value;
+            //    var group = _context.Groups.FirstOrDefault(x => x.TeacherId == userId && (x.YearTo.Year == DateTime.Now.Year || x.YearFrom.Year == DateTime.Now.Year));
+
+            //    var listMarks = new List<IEnumerable<Mark>>();
+            //    for(int i=1; i<=31; i++)
+            //    {
+            //        var data = $"{i}/{model.Month}/{DateTime.Now.Year}";
+            //        var marks = _marks.GetMarksInGroup(group.Id, model.SubjectId, data);
+            //        listMarks.Add(marks);
+            //    }
+            //    return Ok(listMarks);
+            //}
+            //catch(Exception ex)
+            //{
+            //    return BadRequest("Error: " + ex.Message);
+            //}
+            if(model == null)
             {
-                if (!ModelState.IsValid)
-                {
-                    return "Виберіть предмет або місяць";
-                }
+                return BadRequest("Виберіть предмет або місяць");
+            }
+            else
+            {
+                MarksTableModel table = new MarksTableModel();
+                List<MarksRowModel> tableList = new List<MarksRowModel>();
                 var claims = User.Claims;
                 var userId = claims.FirstOrDefault().Value;
                 var group = _context.Groups.FirstOrDefault(x => x.TeacherId == userId && (x.YearTo.Year == DateTime.Now.Year || x.YearFrom.Year == DateTime.Now.Year));
+                int jourId = _context.Journals.FirstOrDefault(t => t.GroupId == group.Id).Id;
+                var jourCols = _context.JournalColumns.Where(t => t.JournalId == jourId && t.Lesson.SubjectId == model.SubjectId);
+                var lessonDates = jourCols.Select(t => t.Lesson.LessonDate).ToList();
+                lessonDates.OrderByDescending(d => d);
+                var students = _context.GroupsToStudents.Where(t => t.GroupId == group.Id).Select(t => t.Student);
 
-                var listMarks = new List<IEnumerable<Mark>>();
-                for(int i=1; i<=31; i++)
+                foreach(var item in students)
                 {
-                    var data = $"{i}/{model.Month}/{DateTime.Now.Year}";
-                    var marks = _marks.GetMarksInGroup(group.Id, model.SubjectId, data);
-                    listMarks.Add(marks);
+                    var studMarks = _context.Marks.Where(t => jourCols.Contains(t.JournalColumn) && t.StudentId == item.Id);
+                    var marksFormatted = new List<string>();
+                    foreach (var date in lessonDates)
+                    {
+                        var cell = studMarks.FirstOrDefault(m => m.JournalColumn.Lesson.LessonDate == date);
+                        if (cell != null)
+                            marksFormatted.Add(cell.Value);
+                        else
+                            marksFormatted.Add(" ");
+                        var baseP = _context.BaseProfiles.FirstOrDefault(t => t.Id == item.Id);
+                        string name = baseP.Name + " " + baseP.LastName + " " + baseP.Surname;
+                        MarksRowModel rowModel = new MarksRowModel
+                        {
+                            Name = name,
+                            Marks = marksFormatted
+                        };
+                        tableList.Add(rowModel);
+                    }
+                    List<string> cols = new List<string>();
+                    cols.Add("#");
+                    cols.Add("ПІБ");
+                    int lenght = lessonDates.Count;
+                    for (int i = 0; i < lenght; i++)
+                    {
+                        cols.Add(lessonDates[i].ToString("dd.MM.yyyy"));
+                    }
+                    table.rows = tableList;
+                    table.columns = cols;
                 }
-                return Ok(listMarks);
-            }
-            catch(Exception ex)
-            {
-                return BadRequest("Error: " + ex.Message);
+
+                return Ok(table);
             }
         }
     }
