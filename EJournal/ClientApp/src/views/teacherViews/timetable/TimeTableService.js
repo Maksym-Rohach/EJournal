206f3c27f0156abf68dc36c7f0c2bbbf30f6dccc
novakvova <novakvova@gmail.com>
2020-04-12 15:17:45 +0300

    some litle changes

--- EJournal/ClientApp/src/views/teacherViews/timetable/TimeTableService.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import {serverUrl} from '../../../config';
-
-export default class TimeTableService {
-    static getTimetable(model) {
-       
-        return axios.post(`${serverUrl}api/teacher/get/timetable`, model)
-    };
-}
