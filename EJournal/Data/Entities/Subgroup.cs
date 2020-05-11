e810baaa39fc7f6431376736212a106a1ec0b32f
Vitaliy <vetalkasmile@gmail.com>
2020-05-11 16:10:47 +0300

    Added some tables

--- EJournal/Data/Entities/Subgroup.cs
+++ EJournal/Data/Entities/Subgroup.cs
@@ -5,11 +5,14 @@ using System.Threading.Tasks;
 
 namespace EJournal.Data.Entities
 {
-    public class LessonType
+    public class Subgroup
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        public ICollection<Lesson> Lessons { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+
+        public ICollection<GroupToStudent> GroupToStudents { get; set; }
     }
 }
