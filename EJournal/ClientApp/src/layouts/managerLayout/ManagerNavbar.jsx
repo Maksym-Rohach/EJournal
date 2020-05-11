ae872a55a2405fa4d5dd03343facbeabce4b8110
Vitaliy <vetalkasmile@gmail.com>
2020-05-11 22:42:33 +0300

    Added layout functions

--- EJournal/ClientApp/src/layouts/managerLayout/ManagerNavbar.jsx
+++ EJournal/ClientApp/src/layouts/managerLayout/ManagerNavbar.jsx
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { Badge, UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem } from 'reactstrap';
 import PropTypes from 'prop-types';
-
 import { AppAsideToggler, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/logo.svg'
-import sygnet from '../../assets/logo.svg'
+import logo from "../../assets/logo3big.png";
+import logoFull from "../../assets/logo3full.png";
 
 const propTypes = {
   children: PropTypes.node,
@@ -16,14 +15,14 @@ const defaultProps = {};
 class ManagerNavbar extends Component {
   render() {
 
-    const { children, ...attributes } = this.props;
+    const { children, image, name, ...attributes } = this.props;
 
     return (
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 25, alt: 'CoreUI Logo' }}
-          minimized={{ src: sygnet, width: 30, height: 30, alt: 'CoreUI Logo' }}
+          full={{ src: logoFull, height: 40, alt: "Ejournal Logo" }}
+          minimized={{ src: logo, height: 40, alt: "Ejournal Logo" }}
         />
         <AppSidebarToggler className="d-md-down-none" display="lg" />
 
@@ -32,12 +31,12 @@ class ManagerNavbar extends Component {
         <Nav className="ml-auto" navbar>          
           <UncontrolledDropdown nav direction="down">
             <DropdownToggle nav>
-              <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="manager@gmail.com" />
+              <img src={image} className="img-avatar" alt={name} />
+              {name}
             </DropdownToggle>
             <DropdownMenu right>
-              <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
-              <DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
-              <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Logout</DropdownItem>
+              <DropdownItem header tag="div" className="text-center"><strong>Акаунт</strong></DropdownItem>
+              <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Вихід</DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
         </Nav>
